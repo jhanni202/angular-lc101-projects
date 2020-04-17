eac1018ed4af12fa8934d70e03d7e76cbe28edd7
Hannigan <jhanni202@cable.comcast.com>
2020-04-17 13:41:55 -0400

    they told me to save and commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://upload.wikimedia.org/wikipedia/en/b/bb/Rick_and_Morty_season_4.png';
+  image3 = 'https://images.wsj.net/im-1803?width=620&size=1.5';
 
   constructor() { }
 
